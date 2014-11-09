7d195299cdfff2deffdd06a3c40a4d1c22a78b55
Flemming Behrend <flemmingjaeger@gmail.com>
2014-11-09 11:56:23 +0100

    removed UserCredentials entity, we now use User entity to hold the user information including password. Also added ui-boostrap.

--- src/main/java/dk/topee/panis/business/login/boundary/LoginResource.java
+++ src/main/java/dk/topee/panis/business/login/boundary/LoginResource.java
@@ -1,32 +1,24 @@
 package dk.topee.panis.business.login.boundary;
 
-import dk.topee.panis.business.login.entity.UserCredentials;
-
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.logging.Logger;
 
 @Path("login")
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginResource {
 
-    private static final Logger log = Logger.getLogger(LoginResource.class.getName());
+//    private static final Logger log = Logger.getLogger(LoginResource.class.getName());
 
     @Inject
     LoginServiceBean loginService;
 
     @GET
-    public UserCredentials getUsername() {
+    public String getUsername() {
         System.out.println("LoginResource.getUsername");
         return loginService.login();
-
     }
 
 }
--- src/main/java/dk/topee/panis/business/login/boundary/LoginServiceBean.java
+++ src/main/java/dk/topee/panis/business/login/boundary/LoginServiceBean.java
@@ -1,28 +1,14 @@
 package dk.topee.panis.business.login.boundary;
 
-import dk.topee.panis.business.login.entity.UserCredentials;
-
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Stateless
 public class LoginServiceBean {
 
-    @PersistenceContext
-    EntityManager em;
-
-    public UserCredentials login() {
+    public String login() {
         System.out.println("LoginServiceBean.login");
 
-        UserCredentials userCredentials = new UserCredentials();
-        userCredentials.setEmail("test@test.dk");
-        userCredentials.setPassword("test");
-
-        em.persist(userCredentials);
-
-        UserCredentials userCredentials1 = em.find(UserCredentials.class, 1);
-        return userCredentials1;
+        return "String";
 
     }
 }
