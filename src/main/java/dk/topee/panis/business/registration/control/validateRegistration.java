44daeae8821df8d85a7f2b0367aacf09d49459b5
Flemming Behrend <flemmingjaeger@gmail.com>
2014-11-09 02:38:05 +0100

    added initial classes for registering a user|

---
+++ src/main/java/dk/topee/panis/business/registration/control/validateRegistration.java
@@ -0,0 +1,12 @@
+package dk.topee.panis.business.registration.control;
+
+import dk.topee.panis.business.registration.entity.User;
+
+public class validateRegistration {
+
+    public boolean isValid(User user) {
+
+        return true;
+    }
+
+}
---
+++ src/main/java/dk/topee/panis/business/registration/entity/Department.java
@@ -0,0 +1,6 @@
+package dk.topee.panis.business.registration.entity;
+
+public enum Department {
+    IT_SKADE,
+    IT_MASSEMARKET
+}
