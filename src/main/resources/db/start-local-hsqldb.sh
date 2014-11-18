645e0435ba9de7ed16bd1ac27e50b0d415e3e862
Flemming Behrend <flemmingjaeger@gmail.com>
2014-11-18 01:08:47 +0100

    changed some configuration files

--- src/main/resources/db/start-local-hsqldb.sh
+++ src/main/resources/db/start-local-hsqldb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # $JAVA_HOME needs to point to an installation that contain javadb
-java -cp %UserProfile%\.m2\repository\org\hsqldb\hsqldb\2.3.1\hsqldb-2.3.1.jar org.hsqldb.Server --props ./server.properties
+java -cp $HOME/.m2/repository/org/hsqldb/hsqldb/2.3.1/hsqldb-2.3.1.jar org.hsqldb.Server --props ./server.properties
